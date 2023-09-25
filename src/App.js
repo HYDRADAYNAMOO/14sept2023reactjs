3de4e40f889918aeebe02b359df25eeffb03eba0
HYDRADAYNAMOO <tarunjoshi5158@gmail.com>
2023-09-26 04:04:53 +0530

    c1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+// 1. importa area
 import logo from './logo.svg';
 import './App.css';
 
+
+//20 function defination area 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Aside />
+        <section>section component</section>
+        </main>
+        <Footer />
   );
-}
-
+    }
+//3. export area
 export default App;
